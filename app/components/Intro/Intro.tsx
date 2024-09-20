c987a9ef08eabebec9fd86615a93a9d4230c2d16
thanghn <ngocthang.tcom@gmail.com>
2024-09-20 14:34:17 +0700

    update 20/09

--- app/components/Intro/Intro.tsx
+++ app/components/Intro/Intro.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import model from "@/assets/imgs/Intro/model3-1.jpg";
+import model from "@/assets/imgs/Intro/modelx.jpg";
 import signature from "@/assets/imgs/Intro/signature.png";
 
 export default function Intro() {
@@ -10,26 +10,27 @@ export default function Intro() {
       </div>
       <div className="lg:max-w-[50%] px-[15px] mt-[50px] lg:mt-0 flex-grow">
         <p className="font-dancing text-4xl sm:text-[52px] text-[#4c4c4c] font-bold leading-10 tracking-[1.2px] text-center md:text-left">
-          Nguyễn Thị Bích Thuỷ
+          Nguyễn Trần Nhiệm
         </p>
         <p className="font-dancing text-[28px] sm:text-[32px] text-[#4c4c4c] font-medium leading-10 tracking-[0.72px] text-center md:text-left mt-5">
-          CEO Trang Huy logistics
+          Giám đốc
         </p>
         {/* <p className="text-lg sm:text-[20px] text-[#4c4c4c] font-medium leading-[30px] italic mt-[45px] text-center md:text-left">
           To get started with SkyMax, please go over all of our plans to let our
           operators know which one works for you the best!
         </p> */}
         <p className="font-dancing text-2xl text-[#808080] leading-[50px] font-light mt-[25px] text-justify">
-          Sứ mệnh của tôi là mang đến giai đoạn chuyển đổi số cho Trang Huy
-          Logistic để thay đổi cách vận hành & quản trị doanh nghiệp. Với kinh
-          nghiệm & năng lượng của người đứng đầu tôi tin sẽ chuyển đổi số thành
-          công cho Trang Huy Logistic. Là người đam mê công nghệ tôi biết công
+          Sứ mệnh của chúng tôi là mang đến giai đoạn chuyển đổi số cho các
+          doanh nghiệp, để thay đổi cách vận hành & quản trị doanh nghiệp. Với
+          kinh nghiệm & năng lượng của người đứng đầu tôi tin sẽ chuyển đổi số
+          thành công cho khách hàng. Là người đam mê công nghệ tôi biết công
           việc chuyển đổi số sẽ gặp rất nhiều khó khăn, trở ngại nhưng tôi lại
-          có một niềm tin mãnh liệt về một tương lai tươi sáng dành cho TrangHuy
-          Logistic. Dành cho những con người dám nghĩ dám làm, nói như ngôn ngữ
-          của người làm công nghệ là “Cứ máu có lẽ là xong”. Vâng rồi sẽ xong !
-          Xong nhiệm vụ này rồi lại có nhiệm vụ khác lớn hơn, nhưng rồi tất cả
-          cũng sẽ được hoàn thành...
+          có một niềm tin mãnh liệt về một tương lai tươi sáng dành cho các
+          doanh nghiệp muốn công nghệ hóa. Tư vấn chuyển đổi số thực sự rất khó
+          khăn & vất vả nhưng với chúng tôi, những con người dám nghĩ dám làm,
+          nói như ngôn ngữ của người làm công nghệ là “Cứ máu có lẽ là xong”.
+          Vâng rồi sẽ xong ! Xong nhiệm vụ này rồi lại có nhiệm vụ khác lớn hơn,
+          nhưng rồi tất cả cũng sẽ được hoàn thành...
         </p>
         <div className="w-full flex justify-end items-center sm:items-start flex-col-reverse sm:flex-row gap-5 mt-[50px]">
           {/* <a
--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Timeline />
       <Careers />
       <Television />
-      <Teams />
+      {/* <Teams /> */}
       <FormQuestion />
       <Blogs />
       <Questions />
